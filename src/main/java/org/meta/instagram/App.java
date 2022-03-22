6cfd7a3340dfe79b3b6a9e2b1c4bb9d5176593f1
saurabdey <saurab.jeet@gmail.com>
2022-03-22 13:05:45 +0530

    my first commit

---
+++ src/main/java/org/meta/instagram/App.java
@@ -0,0 +1,50 @@
+package org.meta.instagram;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    WebDriver driver;
+	public static void main( String[] args )
+	{
+		App ref= new App();
+		ref.setup();
+		ref.login();
+		ref.teardown();
+	}
+	public void setup() 
+	{
+		System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
+		driver= new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+	}
+	public void login()
+	{
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		
+		
+		WebElement pass= driver.findElement(By.id("txtPassword"));
+		pass.sendKeys("admin123");
+		
+		WebElement logbut=	driver.findElement(By.id("btnLogin"));
+		logbut.click();
+	}
+	public void teardown()
+	{
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.quit();
+	}
+
+}
